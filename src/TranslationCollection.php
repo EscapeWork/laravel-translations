f3fa70a8c39d134d90931c03e01097e454bf235d
Luis Dalmolin <luis.nh@gmail.com>
2016-04-11 15:49:14 -0300

    Fixed collection

--- src/TranslationCollection.php
+++ src/TranslationCollection.php
@@ -7,12 +7,12 @@ use Illuminate\Database\Eloquent\Collection;
 class TranslationCollection extends Collection
 {
 
-    public function _get($field, $language)
+    public function _get($field, $locale)
     {
-        $language = $language ?: config('app.locale');
+        $locale = $locale ?: config('app.locale');
 
         foreach ($this->items as $item) {
-            if ($item->language == $language) {
+            if ($item->locale == $locale) {
                 if ($item->{$field}) {
                     return $item->{$field};
                 }
